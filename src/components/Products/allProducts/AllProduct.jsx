45af2b9f112deb703434760c7ce9cf00be88bb0a
BuddhadevBauna <baunabuddhadev2001@gmail.com>
2024-04-30 10:06:25 +0530

    Changd style of product categoy container in differnt screen sizes

--- src/components/Products/allProducts/AllProduct.jsx
+++ src/components/Products/allProducts/AllProduct.jsx
@@ -1,13 +1,17 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import "./AllProduct.css";
+import { useDispatch, useSelector } from "react-redux";
 import { setAllProducts } from "../../../redux/reducers/allProductSlice";
 import axios from "axios";
-import ProductsView from "../productView/ProductsView";
 import { removeProductOfCategory } from "../../../redux/reducers/productsOfCategorySlice";
+import { Link } from "react-router-dom";
+import { IoEyeSharp } from "react-icons/io5";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
 const AllProduct = () => {
-    const dispatch = useDispatch();
+    const products = useSelector((state) => state.allProduct.products);
 
+    const dispatch = useDispatch();
     useEffect(() => {
         const fetchProducts = async () => {
             dispatch(removeProductOfCategory());
@@ -27,11 +31,63 @@ const AllProduct = () => {
         fetchProducts();
     }, [dispatch])
 
+    const [hoverStates, setHoverStates] = useState(products ? new Array(products.length).fill(false) : []);
+    const handleMouseHover = (index) => {
+        const newHoverStates = [...hoverStates];
+        newHoverStates[index] = true;
+        setHoverStates(newHoverStates);
+    }
+    const handleMouseLeave = (index) => {
+        const newHoverStates = [...hoverStates];
+        newHoverStates[index] = false;
+        setHoverStates(newHoverStates);
+    }
+    // console.log(hoverStates);
+
+
+    if (!products) {
+        return <div>Loading...</div>;
+    }
+    const renderList = products.map((product, index) => {
+        let { id, title, images, category, price, discountPercentage, rating } = product;
+        let MRP = price / (1 - (discountPercentage / 100));
+        let MRPInt = Math.round(MRP);
+        return (
+            <Link to={`/products/${id}`} className="product-container" key={id} >
+                <img className="product-img" src={images[0]} alt={title} />
+                <div className="product-body">
+                    <div className="product-stock">
+                        <small>{category}</small>
+                        <i
+                            onMouseOver={() => handleMouseHover(index)}
+                            onMouseLeave={() => handleMouseLeave(index)}
+                        >
+                            {hoverStates[index] ? <FaHeart className="heart"/> : <FaRegHeart />}
+                        </i>
+                    </div>
+                    <div className="product-name">
+                        <p>{title.length > 25 ? title.substring(0, 25) + "..." : title}</p>
+                    </div>
+                    <div className="product-buy">
+                        <p>₹ {price}</p>
+                        <small><span>₹ {MRPInt}</span> {discountPercentage}% off </small>
+                        <small className="rating">{rating} ★</small>
+                    </div>
+                    <div className="product-btn">
+                        <button type="button" className="btn">Add To Cart</button>
+                        <Link to={`/products/${id}`} className="btn view-btn">
+                            <i><IoEyeSharp /></i>
+                        </Link>
+                    </div>
+                </div>
+            </Link>
+        );
+    })
 
     return (
-        <ProductsView 
-            menu = "allProducts"
-        />
+        <div className="grid-container">
+            {renderList}
+        </div>
     );
 }
 
--- src/components/Products/productsOfCategory/ProductsOfCategory.jsx
+++ src/components/Products/productsOfCategory/ProductsOfCategory.jsx
@@ -1,18 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import "./ProductsOfCategory.css";
 import axios from "axios";
-import { useParams } from "react-router-dom"
-import { useDispatch } from "react-redux";
+import { Link, useParams } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux";
 import { setProductsOfCategory } from "../../../redux/reducers/productsOfCategorySlice";
-import ProductsView from "../productView/ProductsView";
+import { FaRegHeart } from "react-icons/fa";
 
 const ProductsOfCategory = () => {
+    const products = useSelector((state) => state.categoryProducts.products);
+    // console.log(products);
+
     const { particularCategory } = useParams();
     // console.log(particularCategory);
-
     const dispatch = useDispatch();
-
     useEffect(() => {
-        const fetchProductsOfCategory = async() => {
+        const fetchProductsOfCategory = async () => {
             try {
                 const response = await axios(`https://dummyjson.com/products/category/${particularCategory}`);
                 // console.log(response);
@@ -25,11 +27,254 @@ const ProductsOfCategory = () => {
         fetchProductsOfCategory();
     }, [particularCategory, dispatch]);
 
+    useEffect(() => {
+        const handaleResize = () => {
+            if (window.innerWidth > 767) {
+                setCategory(true);
+                setBrand(true);
+                setRating(true);
+                setPrice(true);
+            } else {
+                setCategory(false);
+                setBrand(false);
+                setRating(false);
+                setPrice(false);
+            }
+        }
 
+        //Call handaleResize initially
+        handaleResize();
+
+        //add Event Listner for window resize
+        window.addEventListener("resize", handaleResize);
+
+        //CleanUp
+        return () => {
+            window.addEventListener("resize", handaleResize);
+        }
+    }, [])
+
+    const filterContainerRef = useRef(null);
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (filterContainerRef.current && !filterContainerRef.current.contains(event.target)) {
+                if (window.innerWidth > 767) {
+                    setCategory(true);
+                    setBrand(true);
+                    setRating(true);
+                    setPrice(true);
+                } else {
+                    setCategory(false);
+                    setBrand(false);
+                    setRating(false);
+                    setPrice(false);
+                }
+            }
+        };
+        const handleScroll = () => {
+            if (window.innerWidth > 767) {
+                setCategory(true);
+                setBrand(true);
+                setRating(true);
+                setPrice(true);
+            } else {
+                setCategory(false);
+                setBrand(false);
+                setRating(false);
+                setPrice(false);
+            }
+        }
+
+        document.addEventListener("click", handleClickOutside);
+        document.addEventListener("scroll", handleScroll);
+
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+            document.addEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const [isCategory, setCategory] = useState(false);
+    const [isBrand, setBrand] = useState(false);
+    const [isRating, setRating] = useState(false);
+    const [isPrice, setPrice] = useState(false);
+    //for check any of filter active
+    const handleCategory = () => {
+        if (window.innerWidth <= 767) {
+            setCategory(!isCategory);
+            // Another filter deactive
+            setBrand(false);
+            setRating(false);
+            setPrice(false);
+        }
+    }
+    const handleBrand = () => {
+        if (window.innerWidth <= 767) {
+            setBrand(!isBrand);
+            // Another filter deactive
+            setCategory(false);
+            setRating(false);
+            setPrice(false);
+        }
+    }
+    const handleRating = () => {
+        if (window.innerWidth <= 767) {
+            setRating(!isRating);
+            // Another filter deactive
+            setCategory(false);
+            setBrand(false);
+            setPrice(false);
+        }
+    }
+    const handlePrice = () => {
+        if (window.innerWidth <= 767) {
+            setPrice(!isPrice);
+            // Another filter deactive
+            setCategory(false);
+            setBrand(false);
+            setRating(false);
+        }
+    }
+
+    const disableLink = (e) => {
+        if ((isCategory || isBrand || isRating || isPrice) && window.innerWidth <= 767) {
+            e.preventDefault();
+        }
+    }
+
+
+    if (!products) {
+        return <div>Loading...</div>
+    }
+    const renderList = products.map((product, index) => {
+        let { id, title, images, rating, description, price, discountPercentage, stock } = product;
+        let MRP = price / (1 - (discountPercentage / 100));
+        let MRPInt = Math.round(MRP);
+        return (
+            <Link to={`/products/${id}`}
+                key={id}
+                className={`product-menu ${isCategory || isBrand || isRating || isPrice ? 'product-menu-inactive' : ''}`}
+                onClick={(e) => disableLink(e)}
+            >
+                <div className="product-img">
+                    <img src={images[0]} alt="product-img"></img>
+                    <i><FaRegHeart /></i>
+                </div>
+                <div className="product-details">
+                    <div className="product-description">
+                        <h2>{title}</h2>
+                        <p><span className="rating">{rating}★</span> <span>80 Rating & 10 Reviews</span></p>
+                        <p className="description">{description}</p>
+                    </div>
+                    <div className="product-buy">
+                        <p>₹ {price}</p>
+                        <small><span>₹ {MRPInt}</span> {discountPercentage}% off </small>
+                        <small className="stock">only {stock} left</small>
+                        <button>Buy Now</button>
+                    </div>
+                </div>
+            </Link>
+        );
+    })
+
+    const uniqeBrands = [];
+    products.forEach(product => {
+        if(!uniqeBrands.includes(product.brand)) {
+            uniqeBrands.push(product.brand);
+        }
+    });
+    
     return (
-        <ProductsView 
-            menu = "productsOfCategory"
-        />  
+        <div className="product-category-container">
+            <div>
+                <div className="filter-container" ref={filterContainerRef}>
+                    <div className="filter">
+                        <p>Filters</p>
+                        <p>Clear All</p>
+                    </div>
+                    <div className="category">
+                        <p onClick={() => handleCategory()}>CATEGORY</p>
+                        {isCategory &&
+                            <div className="inner-div">
+                                <p>CATEGORY</p>
+                                <ul>
+                                    <li>{particularCategory}</li>
+                                </ul>
+                            </div>
+                        }
+                    </div>
+                    <div className="brand">
+                        <p onClick={() => handleBrand()}>BRAND</p>
+                        {isBrand &&
+                            <div className="inner-div">
+                                <p>BRAND</p>
+                                <ul>
+                                    {
+                                        uniqeBrands.map(brand => {
+                                            return (
+                                                <li>
+                                                    <span>
+                                                        <input type="checkbox"></input> 
+                                                        <menu>{brand}</menu>
+                                                    </span>
+                                                </li>
+                                            );
+                                        })
+                                    }
+                                </ul>
+                                <hr></hr>
+                                <div className="filter-sm">
+                                    <p>Clear Filtes</p>
+                                    <p>Show Results</p>
+                                </div>
+                                <hr></hr>
+                            </div>
+                        }
+                    </div>
+                    <div className="rating">
+                        <p onClick={() => handleRating()}>RATING</p>
+                        {isRating &&
+                            <div className="inner-div">
+                                <p>RATING</p>
+                                <ul>
+                                    <li><span><input type="checkbox"></input> <menu>4★ & above</menu></span></li>
+                                    <li><span><input type="checkbox"></input> <menu>4.5★ & above</menu></span></li>
+                                </ul>
+                                <hr></hr>
+                                <div className="filter-sm">
+                                    <p>Clear Filtes</p>
+                                    <p>Show Results</p>
+                                </div>
+                                <hr></hr>
+                            </div>
+                        }
+                    </div>
+                    <div className="price">
+                        <p onClick={() => handlePrice()}>PRICE</p>
+                        {isPrice &&
+                            <div className="inner-div">
+                                <p>PRICE</p>
+                                <ul>
+                                    <li><span><input type="checkbox"></input> <menu>500 & bellow</menu></span></li>
+                                    <li><span><input type="checkbox"></input> <menu>1000 & bellow</menu></span></li>
+                                    <li><span><input type="checkbox"></input> <menu>1500 & bellow</menu></span></li>
+                                    <li><span><input type="checkbox"></input> <menu>1500 & above</menu></span></li>
+                                </ul>
+                                <hr></hr>
+                                <div className="filter-sm">
+                                    <p>Clear Filtes</p>
+                                    <p>Show Results</p>
+                                </div>
+                                <hr></hr>
+                            </div>
+                        }
+                    </div>
+                </div>
+            </div>
+            <div className="product-container">
+                {renderList}
+            </div>
+        </div>
     );
 }
 
